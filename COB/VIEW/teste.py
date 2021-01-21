55174ee00cb1f232495a61de77eb8e222160abfa
Oscar Niemeyer Netto <nettooscar@me.com>
2021-01-21 17:39:55 -0300

    melhor

--- COB/VIEW/teste.py
+++ COB/VIEW/teste.py
@@ -29,9 +29,21 @@ class teste(object):
 
         self.comboOpcoes = ['EMOP','SCO-RIO','SINAPRI']
 
-        self.comboFiltro = wx.ComboBox(self.panel, wx.ID_ANY, value='ID',pos = (25,135), choices = self.comboOpcoes, style=wx.CB_READONLY)
+        self.comboFiltro = wx.ComboBox(self.panel, wx.ID_ANY, value='ID',pos = (25,60), choices = self.comboOpcoes, style=wx.CB_READONLY)
 
-        self.buttonFiltar = wx.Button(self.panel, wx.ID_ANY, 'Filtar', (25, 525),size=(100,-1))
+        self.buttonFiltar = wx.Button(self.panel, wx.ID_ANY, 'Filtar', (25, 300),size=(100,-1))
+
+
+
+        self.button_excluir = wx.Button(self.panel, wx.ID_ANY, 'Excluir', (25, 690),size=(100,-1))
+        self.buttoninserircodigo = wx.Button(self.panel, wx.ID_ANY, 'Inserir', (400, 250),size=(100,-1))
+        self.button_inserirtabela = wx.Button(self.panel, wx.ID_ANY, 'Inserir', (1350, 250),size=(100,-1))
+        self.button_pesquisar = wx.Button(self.panel, wx.ID_ANY, 'Pesquisar', (750, 250),size=(100,-1))
+        self.caixacodigo = wx.TextCtrl(self.panel, wx.ID_ANY, wx.EmptyString, (250,250), size=(150,-1))
+        self.button_salvar = wx.Button(self.panel, wx.ID_ANY, 'Salvar', (1350, 690),size=(100,-1))
+
+        self.buttonresetar = wx.Button(self.panel, wx.ID_ANY, 'Resetar', (750, 690),size=(100,-1))
+
 
         #wx.StaticText(self.panel, wx.ID_ANY, "Filtra", (25, 25))
         #self.txtFiltro = wx.TextCtrl(self.panel, wx.ID_ANY,"", (60, 25),size=(500, -1),style=wx.TE_PROCESS_ENTER)
