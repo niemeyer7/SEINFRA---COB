5a25c13ba0b9868a5ff4ed042599b2a6c8b2ae17
Oscar Niemeyer Netto <nettooscar@me.com>
2021-02-19 11:01:35 -0300

    dando o mesmo erro

--- COB/LeitorCatalagos.py
+++ COB/LeitorCatalagos.py
@@ -5,14 +5,14 @@ import modelcatalagos
 emop =pd.read_csv("dadosgerais.csv", sep=';', error_bad_lines=False)
 emop = emop.to_records(index=False)
 emop = tuple(emop)
-
+print(emop[1])
 
 sinapi =pd.read_csv("sinapi.csv", sep=';', error_bad_lines=False)
 sinapi = sinapi.to_records(index=False)
 sinapi = tuple(sinapi)
+print(sinapi[1])
 
-for teste in emop:
-    test2 = modelcatalagos.catalagos(listaDeitensCatalago=teste, tipo=2)
 
-print(1)
-print(test2)
+x = modelcatalagos.catalagos(listaDeitensCatalago=emop[1], tipo=2)
+
+print(x)
