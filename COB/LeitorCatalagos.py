62ffe2924c6ac4c41b97902fc021c39bec2ba6f7
Oscar Niemeyer Netto <nettooscar@me.com>
2021-02-19 12:26:30 -0300

    novamodel

--- COB/LeitorCatalagos.py
+++ COB/LeitorCatalagos.py
@@ -2,12 +2,12 @@ import pandas as pd
 import modelcatalagos
 
 
-emop =pd.read_csv("dadosgerais.csv", sep=';', error_bad_lines=False)
+emop =pd.read_csv("dadosgerais.csv", sep=':', error_bad_lines=False)
 emop = emop.to_records(index=False)
 emop = tuple(emop)
 print(emop[1])
 
-sinapi =pd.read_csv("sinapi.csv", sep=';', error_bad_lines=False)
+sinapi =pd.read_csv("sinapi.csv", sep=':', error_bad_lines=False)
 sinapi = sinapi.to_records(index=False)
 sinapi = tuple(sinapi)
 print(sinapi[1])
